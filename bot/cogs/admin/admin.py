89b576db9f6f2592f6d10434c4b6b3840915e469
ojasvi jain <ojasvi.jaain@gmail.com>
2020-10-01 20:26:04 +0600

    Fixed the conflict

--- bot/cogs/admin/admin.py
+++ bot/cogs/admin/admin.py
@@ -7,16 +7,7 @@ class Admin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    @commands.has_permissions(kick_members=True)
-    async def kick(self, ctx, member: discord.Member, *, reason="No Reason!"):
-        await ctx.send(f'Nice try {ctx.author.mention}!')
 
-        if member == None or member == ctx.message.author:
-            await ctx.channel.send("You cannot kick yourself!")
-            return
-        await member.kick(reason=reason)
-        await ctx.send(f'Kicked {member}!')
 
     @commands.command()
     @commands.has_permissions(administrator=True)
@@ -27,6 +18,17 @@ class Admin(commands.Cog):
     async def clear_error(self, ctx, error):
         if isinstance(error, commands.BadArgument):
             await ctx.send("Specify the amount in positive integers please.")
+
+	@commands.command()
+    @commands.has_permissions(kick_members=True)
+    async def kick(self, ctx, member: discord.Member, *, reason="No Reason!"):
+        await ctx.send(f'Nice try {ctx.author.mention}!')
+
+        if member == None or member == ctx.message.author:
+            await ctx.channel.send("You cannot kick yourself!")
+            return
+        await member.kick(reason=reason)
+        await ctx.send(f'Kicked {member}!')
 
     @ commands.command()
     @ commands.has_permissions(ban_members=True)
@@ -39,7 +41,6 @@ class Admin(commands.Cog):
         await member.ban(reason=reason)
         await ctx.send(f'Banned {member}!')
 
-<<<<<<< HEAD
     @commands.command()
     @commands.has_permissions(administrator=True)
     async def unban(self, ctx, *, member):
@@ -54,8 +55,6 @@ class Admin(commands.Cog):
                 await ctx.send(f'Unbanned {user.mention}')
                 return
 
-=======
->>>>>>> parent of 374fe7e... Added unban command
 
 def setup(bot):
     bot.add_cog(Admin(bot))
