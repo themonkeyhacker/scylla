7e905b851a7a4e2ef9e02947a0eb6a5d4a596f4a
ojasvi jain <ojasvi.jaain@gmail.com>
2020-10-01 06:52:55 +0600

    Added ban command

--- bot/cogs/admin/admin.py
+++ bot/cogs/admin/admin.py
@@ -28,6 +28,17 @@ class Admin(commands.Cog):
         if isinstance(error, commands.BadArgument):
             await ctx.send("Specify the amount in positive integers please.")
 
+    @ commands.command()
+    @ commands.has_permissions(ban_members=True)
+    async def ban(self, ctx, member: discord.Member, *, reason=None):
+        if member == None or member == ctx.message.author:
+            await ctx.channel.send("You cannot ban yourself!")
+            # print(author.role.id)
+            return
+
+        await member.ban(reason=reason)
+        await ctx.send(f'Banned {member}!')
+
 
 def setup(bot):
     bot.add_cog(Admin(bot))
